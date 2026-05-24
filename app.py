6f5378410863726a7a17f4ad8b3abe20e556c94d
aditikumari42169-stack <aditikumari42169@gmail.com>
2026-05-24 23:12:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,24 +12,27 @@ st.title("Dog vs Cat Prediction")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
+
     img = Image.open(uploaded_file)
     st.image(img, caption="Uploaded Image", width=500)
 
     img = img.resize((224,224))
+
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array / 255.0
 
-    prediction = model.predict(img_array)
+    try:
+        prediction = model.predict(img_array)
 
-    st.write("Raw Prediction:", prediction)
+        st.write("Raw Prediction:", prediction)
 
-    pred_value = prediction[0][0]
+        pred_value = prediction[0][0]
 
-    if pred_value > 0.5:
-        st.success("Prediction: Cat 🐱")
-    else:
-        st.success("Prediction: Dog 🐶")
+        if pred_value > 0.5:
+            st.success("Prediction: Cat 🐱")
+        else:
+            st.success("Prediction: Dog 🐶")
 
-except Exception as e:
-    st.error(f"Error: {e}")
+    except Exception as e:
+        st.error(f"Error: {e}")
